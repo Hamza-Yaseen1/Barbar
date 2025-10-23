a0efcc5325344f6424c5b3d6c4b44b94e9a89e0a
Hamza-Yaseen1 <hamza.sce1@gmail.com>
2025-10-23 12:17:07 +0500

    Navbar Created

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,17 @@
+const Navbar =()=>{
+   return(
+    <>
+    <nav className="flex justify-between p-8 bg-green-300 ">
+        <div className="pl-40">LOGO</div>
+        <ul className="flex gap-10 list-none pr-20">
+            <li>Home</li>
+            <li>Our Team</li>
+            <li>Gallery</li>
+            <li>Price</li>
+            <li>Contact Us</li>
+        </ul>
+    </nav>
+    </>
+   ) 
+}
+export default Navbar
